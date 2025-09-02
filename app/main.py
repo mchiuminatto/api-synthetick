f357598b481ec0ba07c2612bc15548429e8016a4
Marcello Chiuminatto <mchiuminatto@gmail.com>
2025-09-01 23:16:07 -0300

    Add tests (#1)
    Signed-off-by: Marcello Chiuminatto <mchiuminatto@gmail.com>

--- app/main.py
+++ app/main.py
@@ -9,83 +9,123 @@ from .currency_types import PriceDataRequest, CurrencyPair, TimeFrame
 from . import constants as const
 
 
-
-
 app = FastAPI()
+
 
 @app.get("/")
 async def read_root():
-    return {"Synthetic": "version 0.0.1"}
+    return {"Synthetic": f"version {const.VERSION}"}
 
 
-@app.post("/forex/dataset/")
+@app.post("/forex/request/")
 async def gen_price_by_start_date_and_records(
-                            currency_code: Annotated[CurrencyPair, 
-                                                        Query(title="Currency Code", 
-                                                            description="The currency pair code, e.g., EURUSD, USDJPY.")],
-                            time_frame: Annotated[TimeFrame, 
-                                                    Query(title="Time Frame", 
-                                                        description="The time frame for the data, e.g., M1, H1, D1.")],
-                            start_date: Annotated[datetime, 
-                                                    Query(title="Start Date", 
-                                                        description="The start date for data generation in ISO format.")],
-                            end_date: Annotated[datetime | None, 
-                                                Query(title="End Date", 
-                                                        description="The end date for data generation in ISO format. Optional.")] = None,
-                            records: Annotated[int | None, 
-                                                Query(title="Number of Records", 
-                                                        description="The number of records to generate. Optional, default is 1000.")] = 1000,
-                                        ):
-    """Generate synthetic data for a given currency code, 
-    starting from a specific date, and for a 
+    currency_code: Annotated[
+        CurrencyPair,
+        Query(
+            title="Currency Code",
+            description="The currency pair code, e.g., EURUSD, USDJPY.",
+        ),
+    ],
+    time_frame: Annotated[
+        TimeFrame,
+        Query(
+            title="Time Frame",
+            description="The time frame for the data, e.g., M1, H1, D1.",
+        ),
+    ],
+    start_date: Annotated[
+        datetime,
+        Query(
+            title="Start Date",
+            description="The start date for data generation in ISO format.",
+        ),
+    ],
+    end_date: Annotated[
+        datetime | None,
+        Query(
+            title="End Date",
+            description="The end date for data generation in ISO format. Optional.",
+        ),
+    ] = None,
+    records: Annotated[
+        int | None,
+        Query(
+            title="Number of Records",
+            description="The number of records to generate. Optional, default is 1000.",
+        ),
+    ] = 1000,
+):
+    """Generate synthetic data for a given currency code,
+    starting from a specific date, and for a
     specified number of records."""
-    
+
     return {
-            "symbol": currency_code,
-            "time_frame": time_frame,
-            "start_date": start_date,
-            "end-date": end_date,
-            "records": records,
-            "request_id": ''.join(random.choice(digits + ascii_uppercase) for _ in range(const.REQUEST_ID_SIZE)),
-            "message": "Data generation is not implemented yet."}
+        "symbol": currency_code,
+        "time_frame": time_frame,
+        "start_date": start_date,
+        "end-date": end_date,
+        "records": records,
+        "request_id": "".join(
+            random.choice(digits + ascii_uppercase)
+            for _ in range(const.REQUEST_ID_SIZE)
+        ),
+        "message": "Data generation is not implemented yet.",
+    }
 
 
 @app.get("/forex/status/")
 def get_status(
-    request_id: Annotated[str, 
-                          Query(title="Request ID", 
-                                description="The unique identifier for the request.",
-                                min_length=const.REQUEST_ID_SIZE,
-                                max_length=const.REQUEST_ID_SIZE)],
-                          ):
-    return {"status": "API is running"}
+    request_id: Annotated[
+        str,
+        Query(
+            title="Request ID",
+            description="The unique identifier for the request.",
+            min_length=const.REQUEST_ID_SIZE,
+            max_length=const.REQUEST_ID_SIZE,
+        ),
+    ],
+):
+    return {"status": "API is running",
+            "records-generated": 0}
 
 
 @app.get("/forex/data/")
-def get_data(
-    request_id: Annotated[str, 
-                          Query(title="Request ID", 
-                                description="The unique identifier for the request.",
-                                min_length=const.REQUEST_ID_SIZE,
-                                max_length=const.REQUEST_ID_SIZE)],
-    start_date: Annotated[datetime | None, 
-                          Query(title="Start Date", 
-                                description="The start date for data retrieval in ISO format.")],
-    end_date: Annotated[datetime | None, 
-                        Query(title="End Date", 
-                              description="The end date for data retrieval in ISO format. Optional.")] = None,
-    records: Annotated[int | None, 
-                        Query(title="Number of Records", 
-                              description="The number of records to retrieve. Optional.")] = None,
+def get_actual_data(
+    request_id: Annotated[
+        str,
+        Query(
+            title="Request ID",
+            description="The unique identifier for the request.",
+            min_length=const.REQUEST_ID_SIZE,
+            max_length=const.REQUEST_ID_SIZE,
+        ),
+    ],
+    start_date: Annotated[
+        datetime | None,
+        Query(
+            title="Start Date",
+            description="The start date for data retrieval in ISO format.",
+        ),
+    ],
+    end_date: Annotated[
+        datetime | None,
+        Query(
+            title="End Date",
+            description="The end date for data retrieval in ISO format. Optional.",
+        ),
+    ] = None,
+    records: Annotated[
+        int | None,
+        Query(
+            title="Number of Records",
+            description="The number of records to retrieve. Optional.",
+        ),
+    ] = None,
 ):
     return {
-            "request_id": request_id,
-            "start_date": start_date,
-            "end-date": end_date,
-            "records": records,
-            "message": "Data retrieval is not implemented yet."}
-    
-    
-
-  
-    
+        "request_id": request_id,
+        "start_date": start_date,
+        "end-date": end_date,
+        "records": records,
+        "message": "Data retrieval is not implemented yet.",
+    }
---
+++ tests/test_happy_path.py
@@ -0,0 +1,68 @@
+import random
+from string import ascii_uppercase, digits
+
+from app.main import app
+from fastapi.testclient import TestClient
+import app.constants as const
+
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Synthetic": f"version {const.VERSION}"}
+
+
+def test_request_data_date_records():
+    response = client.post(
+        "/forex/request/?currency_code=EURUSD&time_frame=M1&start_date=2023-01-01T00:00:00&records=500"
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["symbol"] == "EURUSD"
+    assert data["time_frame"] == "M1"
+    assert data["start_date"] == "2023-01-01T00:00:00"
+    assert data["records"] == 500
+    assert len(data["request_id"]) == const.REQUEST_ID_SIZE
+    assert data["message"] == "Data generation is not implemented yet."
+
+
+def test_request_data_date_end_date():
+    response = client.post(
+        "/forex/request/?currency_code=USDJPY&time_frame=H1&start_date=2023-01-01T00:00:00&end_date=2023-01-10T00:00:00"
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["symbol"] == "USDJPY"
+    assert data["time_frame"] == "H1"
+    assert data["start_date"] == "2023-01-01T00:00:00"
+    assert data["end-date"] == "2023-01-10T00:00:00"
+    assert data["records"] == 1000  # Default value
+    assert len(data["request_id"]) == const.REQUEST_ID_SIZE
+    assert data["message"] == "Data generation is not implemented yet."
+
+
+def test_get_status():
+    request_id_mock = "".join(
+            random.choice(digits + ascii_uppercase)
+            for _ in range(const.REQUEST_ID_SIZE))
+            
+    response = client.get(f"/forex/status/?request_id={request_id_mock}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["status"] == "API is running"
+    assert data["records-generated"] == 0
+
+
+def test_get_actual_data():
+    request_id_mock = "".join(
+            random.choice(digits + ascii_uppercase)
+            for _ in range(const.REQUEST_ID_SIZE))
+            
+    response = client.get(f"/forex/data/?request_id={request_id_mock}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["data"] == []
+    assert data["message"] == "Data retrieval is not implemented yet."
