4c05bc6f8d92e2a6c6bdb5a268bb94ffedef02b0
Marcello Chiuminatto <mchiuminatto@gmail.com>
2025-09-25 10:30:16 -0300

    WIP: Integration with synthetick
    Signed-off-by: Marcello Chiuminatto <mchiuminatto@gmail.com>

--- app/services/price_services.py
+++ app/services/price_services.py
@@ -1,97 +1,87 @@
 import asyncio
+
+import pandas as pd
 from synthetick import synthetick
 from app.currency_types import Trend, TimeFrame, InstrumentType, PriceSide
 from app.common import constants as const
 import abc
+from dataclasses import dataclass
+from datetime import datetime
 
-
+@dataclass
 class PriceDaSetSpecification:
-    def __init__(self,
-                 symbol: str,
-                 start_date: str,
-                 end_date: str | None = None,
-                 trend: float | None = 0.0,
-                 volatility_range: float | None = 0.01,
-                 spread_min: float = 0.1,
-                 spread_max: float = 1.0,
-                 remove_weekend: bool = True,
-                 records: int | None = 0,
-                 instrument_type: InstrumentType = InstrumentType.FOREX,
-                 frequency: TimeFrame = TimeFrame.M1,
-                 price_side: PriceSide = PriceSide.BID
-                 ):
-        self.symbol: str = symbol
-        self.start_date: str = start_date
-        self.end_date: str | None = end_date
-        self.trend: float | None = trend
-        self.volatility_range: float | None = volatility_range
 
-        self.spread_min: float = spread_min
-        self.spread_max: float = spread_max
-
-        self.remove_weekend: bool = remove_weekend
-        self.records: int | None = records
-        self.instrument_type: InstrumentType = instrument_type
-        self.frequency: TimeFrame = frequency
-        self.price_side: PriceSide = price_side
+    symbol: str
+    trend: float
+    volatility_range: float
+    spread_min: float
+    spread_max: float
+    remove_weekend: bool
+    records: int
+    instrument_type: InstrumentType
+    frequency: TimeFrame
+    pip_position: int
+    price_side: PriceSide | None = None
 
 
-# TODO: move thos factory pattern to synthetick library in the future
+# TODO: move this factory pattern to synthetick library in the future
 
-class PriceDataSet(abc.ABC):
+class PriceGenerator(abc.ABC):
 
     @abc.abstractmethod
     def produce(self, date_from: str, date_to: str, init_value: float):
         pass
 
 
-class TickPriceDataSet(PriceDataSet):
+class TickPriceDataSet(PriceGenerator):
 
     def __init__(self, specification: PriceDaSetSpecification):
         self.specification = specification
 
-        self.ticks: synthetick.Ticks = synthetick.Ticks(trend=specification.trend,
-                                                        volatility_range=specification.volatility_range,
-                                                        spread_range=[specification.spread_min, specification.spread_max],
-                                                        pip_position=specification.pip_position,
-                                                        remove_weekend=specification.remove_weekend,
-                                                        )
+        self.ticks: synthetick.Ticks = synthetick.Ticks(
+            trend=specification.trend,
+            volatility_range=specification.volatility_range,
+            spread_min=specification.spread_min,
+            spread_max=specification.spread_max,
+            pip_position=specification.pip_position,
+            remove_weekend=specification.remove_weekend,
+        )
 
-    def produce(self, date_from: str, date_to: str, init_value: float):
-        self.ticks.produce(date_from=date_from, date_to=date_to, init_value=init_value)
-        return self.ticks.ticks_time_series
+    def produce(self, date_from: datetime, date_to: datetime, init_value: float) -> pd.DataFrame:
+        self.ticks.produce(date_from=date_from, date_to=date_to,
+                           frequency=const.TICK_FREQUENCY,
+                           init_value=init_value)
+        return self.ticks.price_time_series
 
 
-class PriceDataSetFactory:
+
+class OHLCPriceDataSet(PriceGenerator):
+
+    def __init__(self, specification: PriceDaSetSpecification):
+        self.specification = specification
+
+        self.ohlc: synthetick.OHLC = synthetick.OHLC(
+            trend=specification.trend,
+            volatility_range=specification.volatility_range,
+            spread_min=specification.spread_min,
+            spread_max=specification.spread_max,
+            pip_position=specification.pip_position,
+            remove_weekend=specification.remove_weekend,
+            tick_frequency=const.TICK_FREQUENCY,
+            time_frame=specification.frequency
+        )
+
+    def produce(self, date_from: datetime, date_to: datetime, init_value: float):
+        self.ohlc.produce(date_from=date_from, date_to=date_to, init_value=init_value)
+        return self.ohlc.ohlc_time_series[self.specification.price_side.value]
+
+class PriceGeneratorFactory:
 
     @staticmethod
-    def create_price_data_set(specification: PriceDaSetSpecification) -> PriceDataSet:
+    def create_price_data_set(specification: PriceDaSetSpecification) -> PriceGenerator:
         if specification.frequency == TimeFrame.TICK:
-            return synthetick.Ticks(ternd=specification.trend,
-                                    volatility_range=specification.volatility_range,
-                                    spread_min=specification.spread_min,
-                                    spread_max=specification.spread_max,
-                                    remove_weekend=specification.remove_weekend,
-                                    instrument_type=specification.instrument_type.value,
-                                    )
+            return TickPriceDataSet(specification)
         else:
             return OHLCPriceDataSet(specification)
 
 
-class PriceGenerator:
-
-    def __init__(self,
-                 dataset_specification: PriceDaSetSpecification
-                 ):
-        self.specification: PriceDaSetSpecification = dataset_specification
-        self.tick_frequency = const.TICK_FREQUENCY
-        self.price_generator: synthetick.PriceGenerator = synthetick.PriceGenerator(
-            trend=self.specification.trend,
-            volatility_range=self.specification.volatility_range,
-            spread_min=self.specification.spread_min,
-            spread_max=self.specification.spread_max,
-            remove_weekend=self.specification.remove_weekend,
-            instrument_type=self.specification.instrument_type.value,
-            frequency=self.specification.frequency.value,
-            price_side=self.specification.price_side.value
-        ))
--- tests/unit/services/test_price_services_happy_path.py
+++ tests/unit/services/test_price_services_happy_path.py
@@ -1,25 +1,30 @@
-from app.services.price_services import PriceGenerator
+from app.services.price_services import PriceDaSetSpecification, PriceGeneratorFactory
 from app.currency_types import Trend, TimeFrame, InstrumentType, PriceSide
+from datetime import datetime
 
 
 class TestPriceGeneration:
     def test_generate_tick_small_range(self):
-        generator: PriceGenerator = PriceGenerator(trend=Trend.FLAT,
-                                                   volatility_range=0.01,
-                                                   spread_min=0.1,
-                                                   spread_max=1,
-                                                   remove_weekend=False,
-                                                   instrument_type=InstrumentType.FOREX,
-                                                   frequency=TimeFrame.TICK
-                                                   )
+        price_spec: PriceDaSetSpecification = PriceDaSetSpecification(
+            symbol="EURUSD",
+            trend=Trend.FLAT,
+            volatility_range=0.01,
+            spread_min=1,
+            spread_max=2,
+            remove_weekend=False,
+            records=0,
+            instrument_type=InstrumentType.FOREX,
+            frequency=TimeFrame.TICK,
+            pip_position=-4
+        )
+        generator = PriceGeneratorFactory().create_price_data_set(price_spec)
         data_set = generator.produce(date_from="2023-01-01 00:00:00", date_to="2023-01-02 00:00:00", init_value=1.300)
 
         assert data_set is not None
-        assert data_set.index[0] == "2023-01-01 00:00:00"
-        assert data_set.index[-1] == "2023-01-02 00:00:00"
-        assert data_set["ask"]-data_set["bid"].min() >= 0.1
-        assert data_set["ask"]-data_set["bid"].max() <= 1
-
+        assert data_set.index[0] == datetime.strptime("2023-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
+        assert data_set.index[-1] == datetime.strptime("2023-01-02 00:00:00", "%Y-%m-%d %H:%M:%S")
+        assert float((data_set["ask"] - data_set["bid"]).round(4).min()) >= .0001
+        assert float((data_set["ask"] - data_set["bid"]).round(4).max()) <= .0002
 
     def test_generate_tick_large_range(self):
         generator: PriceGenerator = PriceGenerator(trend=Trend.FLAT,
@@ -55,7 +60,6 @@ class TestPriceGeneration:
         assert data_set.index[-1] == "2023-01-02 00:00:00"
         assert data_set.columns.to_list() == ["open", "high", "low", "close"]
 
-
     def test_generate_ohlc_large_range(self):
         generator: PriceGenerator = PriceGenerator(trend=Trend.FLAT,
                                                    volatility_range=0.01,
@@ -73,5 +77,3 @@ class TestPriceGeneration:
         assert data_set.index[0] == "2023-01-01 00:00:00"
         assert data_set.index[-1] == "2023-01-02 00:00:00"
         assert data_set.columns.to_list() == ["open", "high", "low", "close"]
-
-
